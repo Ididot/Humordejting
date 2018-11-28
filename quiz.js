b83e5885274cd16525b9116facffc88de13e29c7
GeneralSpecifik <44947192+GeneralSpecifik@users.noreply.github.com>
2018-11-28 17:24:42 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,41 @@
 // Quiz cards
+
+
+ // GO back knapp: onlick 
+
+
+
+import React, { Component } from 'react';
+import types from './types.json';
+import './quiz.css';
+
+
+class Quiz extends Component {
+
+    render() {
+    
+    constructor(props) 
+    
+    {
+      super(props);
+      this.state = {currentPage: 'quiz'};
+    }
+
+
+// egen knapp, ej i meny fält
+  changePage(sida) // ------> GO BACK to quizlist
+    {
+      this.setState({currentPage: quizlist}); 
+    }
+
+
+    //console.log(types);
+
+      return <div>
+        <h1>QUIZ</h1>
+
+        </div>;
+
+    }  
+}
+export default Quiz;
