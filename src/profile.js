88fb2b10615bdd7cb38bcc44cfbd4c5480e280ff
GeneralSpecifik <44947192+GeneralSpecifik@users.noreply.github.com>
2018-12-05 22:08:05 +0100

    Update profile.js
    NN: Rättad 5/12-2018
    Knappen som kopplar med matchsidan funkar ej.
    Profilen har flera funktioner som skall föras över till "matchlistan".
    Profilen skall få "profil.ID" från matchlistan så att bara en profil visas.

--- src/profile.js
+++ src/profile.js
@@ -14,26 +14,6 @@ class Profiles extends React.Component {
         }
     }
 
-//------------------------------------
-/*
-    constructor(){
-        super(); //ger this kontexten av dess komponent, inte parents
-        this.state = {
-            currentProfile: 0
-        }
-        this.onClick = this.handleClick.bind(this);
-    }
-
-// Klicka för att byta profil
-    handleClick(event){
-        this.setState({
-                currentProfile: this.state.currentProfile+1
-        });
-    }
-
-*/
-//------------------------------------
-
 
     render() {
 
@@ -53,7 +33,7 @@ class Profiles extends React.Component {
 
                             <div>
 
-                                <img   src={"./img/./prof/"+currentProfile.img} key="{currentProfile.profileID}"/>
+                                <img src={"./img/./prof/"+currentProfile.img} key="{currentProfile.profileID}"/>
 
                                 <div> Name: {currentProfile.name} </div>
                                 <div> Bio: {currentProfile.bio} </div>
@@ -66,10 +46,11 @@ class Profiles extends React.Component {
                 })
             }
 
+            <button onClick={() => this.setState({currentPage: 'match'}).bind(this)}> 
+            match
+            </button> 
 
             </div> 
         );
     }  
-} export default Profiles ;
-
-
+} export default Profiles;
