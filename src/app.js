b40df7b8b5b94311f6fe40d3d3906b8aeb1da4dc
GeneralSpecifik <44947192+GeneralSpecifik@users.noreply.github.com>
2018-12-05 21:56:14 +0100

    Update app.js
    Rättad 5/12-2018
    NN

--- src/app.js
+++ src/app.js
@@ -2,48 +2,36 @@ import React, { Component } from 'react';
 import './app.css';
 import Match from './match';
 import Profile from './profile';
-//import Nav from './nav';
-
 
 
 class App extends Component {
+  
 //------------------------------------
   constructor(props) 
-    {
+  {
       super(props);
       this.state = ({currentPage: 'profile'});
     }
 
 
  changePage(sida)
-    {
-      this.setState({currentPage: sida});
-   
-  
-    }
-
+ {
+   this.setState({currentPage: sida});
+ }
 
 //------------------------------------
 
-
-
   render() {
-      
-
-    //return( <Nav/>)
-      
-
-     if (this.state.currentPage == 'profile') 
-    {
-      return( <Profile changePage="??"/>);
-    }
     
-    else if (this.state.currentPage == 'match')
-    {
-      return (<Match/>);
-    }
-
-
+      if (this.state.currentPage == 'profile') 
+      {
+        return( <Profile changePage="sida"/>);
+      }
+    
+      else if (this.state.currentPage == 'match')
+      {
+        return (<Match/>);
+      }
     }
 }
   export default App;
