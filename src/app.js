96c1060fb2bf9f2756496f7a0806bb1fc8ed3ec9
diem-an <45423699+diem-an@users.noreply.github.com>
2018-12-07 16:41:20 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './app.css';
 import Match from './match';
 import Profile from './profile';
+import Quiz from './quiz';
 
 
 class App extends Component {
@@ -10,20 +11,30 @@ class App extends Component {
   constructor(props) 
   {
       super(props);
-      this.state = ({currentPage: 'profile'});
-    }
+      this.state = {
+        currentPage: 'quiz',
+        txt: 'Quiz'
+      };
 
+      this.changePage = this.changePage.bind(this);
+  }
 
  changePage(sida)
  {
-   this.setState({currentPage: sida});
- }
+   this.setState({
+      currentPage: sida
+    });
 
-//------------------------------------
+ }
 
   render() {
     
-      if (this.state.currentPage == 'profile') 
+      if (this.state.currentPage == 'quiz') 
+      {
+        return( <Quiz changePage={this.changePage}/>);
+      }
+      
+      else if(this.state.currentPage == 'profile')
       {
         return( <Profile changePage="sida"/>);
       }
