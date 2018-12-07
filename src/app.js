aaebcc1524d0deb47b62c6ac48b21a3db583b25d
diem-an <45423699+diem-an@users.noreply.github.com>
2018-12-07 18:13:32 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,67 +1,53 @@
-import React from 'react';
-import memes from './quiz.json';
-import ProgressBar from './progressbar';
+import React, { Component } from 'react';
+import './app.css';
+import Match from './match';
+import Profile from './profile';
+import Quiz from './quiz';
+import Welcome from './welcome';
 
 
-class Quiz extends React.Component {
+class App extends Component {
+  
+//------------------------------------
+  constructor(props) 
+  {
+      super(props);
+      this.state = {
+        currentPage: 'welcome',
+        txt: 'Quiz'
+      };
 
-constructor(props){
-	super(props);
-	this.state = {
-		currentImage: 0,
-		memeProgression: 0
-	}
+      this.changePage = this.changePage.bind(this);
+  }
 
-	this.onClick = this.handleClick.bind(this);
+ changePage(sida)
+ {
+   this.setState({
+      currentPage: sida
+    });
+
+ }
+
+  render() {
+    
+      if (this.state.currentPage == 'quiz') 
+      {
+        return( <Quiz changePage={this.changePage}/>);
+      }
+      
+      else if(this.state.currentPage == 'profile')
+      {
+        return( <Profile changePage="sida"/>);
+      }
+    
+      else if (this.state.currentPage == 'match')
+      {
+        return (<Match changePage={this.changePage}/>);
+      }
+      else if (this.state.currentPage == 'welcome')
+      {
+        return (<Welcome changePage={this.changePage}/>);
+      }
+    }
 }
-
-handleClick(event){
-
-	if(this.state.currentImage < memes.length-1)
-	{
-		this.setState({
-			currentImage: this.state.currentImage+1,
-			memeProgression: this.state.memeProgression+1
-		});
-	}
-	else
-	{
-		this.props.changePage('match')
-	}
-}
-
-render(){ 
-
-	console.log(memes)
-
-	let t = this.state.currentImage
-	let m = this.state.memeProgression
-
-	return (
-		<div>
-			<ProgressBar memeProgression={m}/>
-			<img onClick= {this.onClick} src={"./img/icon/tummenupp.png"} src={"./img/quiz/"+memes[t].image}/>
-			<img onClick= {this.onClick} src={"./img/icon/tummenupp.png"}/>
-			<img onClick= {this.onClick} src={"./img/icon/tummenner.png"}/>
-		</div>
-	)
-}
-
-
-/*
-{
-	memes.map((meme) => {
-		return(
-			<div>
-				<img src={"./img/quiz/"+meme.image} key={meme.memeID} />
-				<div>Bildtext: {meme.bio}</div>
-			</div>
-		);
-	})
-}
-*/
-
-
-}
-
-export default Quiz
+  export default App;
