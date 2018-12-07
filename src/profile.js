252f3bb22fbc20b7b93951ae1df4c92e230469a1
GeneralSpecifik <44947192+GeneralSpecifik@users.noreply.github.com>
2018-12-07 17:57:15 +0100

    Update profile.js
    Blockstruktur  i css tillagd
    Kopplar fortfarande inte till match

--- src/profile.js
+++ src/profile.js
@@ -2,55 +2,73 @@
 import React, { Component } from 'react';
 import App from './app';
 import profiles_dtb from './profiles_dtb.json';
+import './profile.css' 
+//https://medium.com/deemaze-software/css-grid-layout-crossed-sections-fca9e956e725
+//https://medium.com/deemaze-software/css-grid-responsive-layouts-and-components-eee1badd5a2f
 
 
 class Profiles extends React.Component {
 
 
     constructor(){
-        super(); 
+        super(); //ger this kontexten av dess komponent, inte parents
         this.state = {
-            currentProfile: 0
+            currentProfile: 0 // Värdet påverkar vilken profil som visas
         }
     }
 
 
+   
+   //FÅ ID FRÅN MATCH
+
     render() {
 
-        console.log(profiles_dtb);
 
-        let t = this.state.currentProfile;
+    console.log(profiles_dtb);
+
+
+    const t = profiles_dtb[this.state.currentProfile]; // Värdet från constructor avgör profilen
+    
+
+    // Kommentera bort "const t" ovan för att använda denna kod ist.
+    /*
+    // ID = 4; 
+    //const t = profiles_dtb[ID];
+    */
 
 		return (
 
-            <div> 
+            <div class='container_P'> 
 
-                <h1>Profile</h1>
+                <div class='header_P'> {t.name}'s Profile </div>
               
-            {
-                profiles_dtb.map((currentProfile) => {
-                    return (
 
-                            <div>
+                    <div class='photo_P'>
+                    <img  src={"./img/./prof/"+t.img} key="{currentProfile.profileID}"/>
+                    </div>
 
-                                <img src={"./img/./prof/"+currentProfile.img} key="{currentProfile.profileID}"/>
-
-                                <div> Name: {currentProfile.name} </div>
-                                <div> Bio: {currentProfile.bio} </div>
-                                <div> Gender:{currentProfile.gender} </div>
-                                <div> Age: {currentProfile.age} </div>
-                                <div> Location: {currentProfile.location} </div>
-
-                            </div>
-                    );
-                })
-            }
-
-            <button onClick={() => this.setState({currentPage: 'match'}).bind(this)}> 
-            match
-            </button> 
+                    <div class='name_P'> Name: {t.name} </div>
+                    <div class='bio_P'> Bio: {t.bio} </div>
+                    <div class='gender_P'> Gender:{t.gender} </div>
+                    <div class='age_P'> Age: {t.age} </div>
+                    <div class='location_P'> Location: {t.location} </div>
 
             </div> 
         );
     }  
 } export default Profiles;
+
+
+
+    
+/*
+    // Gammal kod. Ta ej bort, använder den som referens.
+    let ID = 2;
+
+    const img = profiles_dtb[ID].img
+    const name = profiles_dtb[ID].name
+    const bio = profiles_dtb[ID].bio
+    const gender = profiles_dtb[ID].gender
+    const age = profiles_dtb[ID].age
+    const location = profiles_dtb[ID].location
+    */
