3c3080e0d42be3c0e63aa2146828499c09ffb14b
GeneralSpecifik <44947192+GeneralSpecifik@users.noreply.github.com>
2018-12-07 18:04:48 +0100

    Update match.js

--- src/match.js
+++ src/match.js
@@ -1,19 +1,63 @@
+//Profile
 import React, { Component } from 'react';
-import './match.css';
 import App from './app';
-import Profile from './profile'
+import profiles_dtb from './profiles_dtb.json';
+import Profile from './profile.js';
+import './match.css'
 
-class Match extends Component {
+
+
+
+class Match extends React.Component {
+
+
+    constructor(){
+        super(); 
+        this.state = {
+            currentProfile: 0
+        };
+    }
+
 
     render() {
 
-      return (
 
-      	<div>
-        <h1>MATCH</h1>
-        </div>
-	      
-	)
+        console.log(profiles_dtb);
+        let ID = 4; // testgrej
+
+		return (
+         
+
+            <div  class='container_M'> 
+
+                 <div class='header_M'> Match </div>
+                <p> ID-nummer: {profiles_dtb[ID].profileID} </p> 
+            {
+                profiles_dtb.map((currentProfile) => {
+                    return (
+
+                            <div>
+
+                                <div class='photo_M'>
+                                <img src={"./img/./prof/"+currentProfile.img} key="{currentProfile.profileID}"/>
+                                </div>
+
+                                <div class='name_M'> Name: {currentProfile.name} </div>
+                                <div class='bio_M'> Bio: {currentProfile.bio} </div>
+                                <div class='gender_M'> Gender:{currentProfile.gender} </div>
+                                <div class='age_M'> Age: {currentProfile.age} </div>
+                                <div class='location_M'> Location: {currentProfile.location} </div>
+
+                            </div>
+                    );
+
+                })
+            }
+
+            </div>
+
+        );
+
     }  
-}
-export default Match;
+} export default Match;
+
