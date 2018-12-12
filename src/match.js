579c08b3a893a5eefc765e4db09416140759f432
GeneralSpecifik <44947192+GeneralSpecifik@users.noreply.github.com>
2018-12-12 17:01:38 +0100

    Update match.js

--- src/match.js
+++ src/match.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react';
+import App from './app';
 import profiles_dtb from './profiles_dtb.json';
 import Profile from './profile.js';
 import MatchMini from './matchmini.js';
+import ResultBar from './resultbar';
 import './match.css'
+
+
+
 
 class Match extends React.Component {
 
 
-constructor(){
-    super(); 
-    this.state = {
-    };
-}
+    constructor(){
+        super(); 
+        this.state = {
+            matchResult: 6 //NN - kopplar med resultbar (~line 49)
+        };
+    }
 
-render() {
 
-    return (
+    render() {
 
-        <div className='container_M'> 
 
-            <div className='header_M'> Match </div>
+        //console.log(profiles_dtb);
+        let m = this.state.matchResult; //NN
+      
+		return (
+         
+            <div  className='container_M'> 
 
-            {
-            profiles_dtb.map((currentProfile) => {
-                return <MatchMini key={currentProfile.profileID} profileID={currentProfile.profileID} select={this.props.select}/>})
+                 <div className='header_M'> Match </div>
+            
+                {
+                profiles_dtb.map((currentProfile) => {
+
+                    
+                    return (
+
+                            <div>
+
+                                <div className='photo_M'>
+                                <MatchMini key={currentProfile.profileID} profileID={currentProfile.profileID} select={this.props.select}/>
+                                </div>
+
+                                <div  className='prog_M'>
+                                <ResultBar matchResult={m--}/>
+                                </div>
+
+                            </div>
+                    );
+
+                })
             }
 
-        </div>
-    );
-}  
+            </div>
+        );
 
+    }  
 } export default Match;
-
