674fa9a7645fa32a2c6cbc488e367dd5168fba02
diem-an <45423699+diem-an@users.noreply.github.com>
2018-12-12 15:00:59 +0100

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -1,57 +1,41 @@
-//Profile
 import React, { Component } from 'react';
-import App from './app';
 import profiles_dtb from './profiles_dtb.json';
 import './profile.css' 
-//https://medium.com/deemaze-software/css-grid-layout-crossed-sections-fca9e956e725
-//https://medium.com/deemaze-software/css-grid-responsive-layouts-and-components-eee1badd5a2f
-
 
 class Profiles extends React.Component {
 
-
     constructor(){
-        super(); //ger this kontexten av dess komponent, inte parents
+        super(); 
         this.state = {
-            currentProfile: 0 // Värdet påverkar vilken profil som visas
         }
+        this.onClick = this.handleClick.bind(this);
     }
 
-
-   
-   //FÅ ID FRÅN MATCH
+    handleClick(event){
+        this.props.changePage('match')
+    }
 
     render() {
 
-
-    console.log(profiles_dtb);
-
-
-    const t = profiles_dtb[this.state.currentProfile]; // Värdet från constructor avgör profilen
-    
-
-    // Kommentera bort "const t" ovan för att använda denna kod ist.
-    /*
-    // ID = 4; 
-    //const t = profiles_dtb[ID];
-    */
+    const t = profiles_dtb[this.props.profileID]; 
 
 		return (
 
-            <div class='container_P'> 
+            <div className='container_P'> 
 
-                <div class='header_P'> {t.name}'s Profile </div>
-              
+                <div className='header_P'> {t.name}'s Profile </div>
 
-                    <div class='photo_P'>
-                    <img  src={"./img/./prof/"+t.img} key="{currentProfile.profileID}"/>
+                    <div className='photo_P'>
+                     <img  src={"./img/./prof/"+t.img}/>
                     </div>
 
-                    <div class='name_P'> Name: {t.name} </div>
-                    <div class='bio_P'> Bio: {t.bio} </div>
-                    <div class='gender_P'> Gender:{t.gender} </div>
-                    <div class='age_P'> Age: {t.age} </div>
-                    <div class='location_P'> Location: {t.location} </div>
+                    <div className='name_P'> Name: {t.name} </div>
+                    <div className='bio_P'> Bio: {t.bio} </div>
+                    <div className='gender_P'> Gender:{t.gender} </div>
+                    <div className='age_P'> Age: {t.age} </div>
+                    <div className='location_P'> Location: {t.location} </div>
+
+                    <img onClick= {this.onClick} src={"./img/icon/tummenner.png"}/>
 
             </div> 
         );
@@ -60,15 +44,4 @@ class Profiles extends React.Component {
 
 
 
-    
-/*
-    // Gammal kod. Ta ej bort, använder den som referens.
-    let ID = 2;
-
-    const img = profiles_dtb[ID].img
-    const name = profiles_dtb[ID].name
-    const bio = profiles_dtb[ID].bio
-    const gender = profiles_dtb[ID].gender
-    const age = profiles_dtb[ID].age
-    const location = profiles_dtb[ID].location
-    */
+   
