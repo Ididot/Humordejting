14e9955dfe2fd91580fe22be2394bc7261a9ab8b
diem-an <45423699+diem-an@users.noreply.github.com>
2018-12-15 16:26:56 +0100

    Update progressbar.js

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import './progressbar.css';
-
-const barWidth = 10;
-
-const pixelsPerDegree = 16;
+import memes from './quiz.json'; //memes.length
 
 class ProgressBar extends React.Component {
 
-render(){ 
+  render() {
 
-	const memeProgression = this.props.memeProgression;
-    let barWidth = memeProgression * pixelsPerDegree;
+    const memeProgression = this.props.memeProgression;
+    let barWidth = (67/memes.length)*(memeProgression+1)+'vw';
    	const c = {width: barWidth};
 
-    barWidth = Math.min(barWidth, barWidth);
-    barWidth = Math.max(barWidth);
+    return (<div className="progress">
 
-	return (
-		<div className="progress">
 	        <div className="progressBar" style={c}>
-	            {memeProgression}
-	         </div>
+	    
+	        </div>
         </div>
-	)
-}
 
+        ); 
+  }
 }
 
 export default ProgressBar
+
