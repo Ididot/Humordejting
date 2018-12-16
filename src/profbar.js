a51b54f0636e8663230d3a6f04c2866ee6061a02
diem-an <45423699+diem-an@users.noreply.github.com>
2018-12-16 17:32:15 +0100

    Update profbar.js

--- src/profbar.js
+++ src/profbar.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import './profbar.css';
+import profiles_dtb from './profiles_dtb.json';
 
+class ProfBar extends React.Component {
+  render() {
+
+    const matchResult = this.props.matchResult;
+    let barWidth = 88 - ((88/profiles_dtb.length)*(matchResult))+'vw';
+    const c = {width: barWidth};
+
+    //88 = 100%
+    //((88/profiles_dtb.length)*(matchResult)) = den procenten som försvinner
+
+    let matchPercent = ;
+
+    return (
+
+        <div>
+            <p className='yourMatch'>{matchPercent}% match:</p>
+
+            <div className="bar">
+
+                <div className="profbar" style={c}></div>
+
+            </div>
+        </div>
+
+        ); 
+  }
+
+}
+export default ProfBar
+
+/*
 const barWidth = 100;
 
 const steps = 10;
@@ -23,5 +55,4 @@ render(){
             </div>
         )
     }
-}
-export default ProfBar
+*/
