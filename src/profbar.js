f870041a0e292d8eef39ce2c2dd819d3a8e9a3d2
diem-an <45423699+diem-an@users.noreply.github.com>
2018-12-17 13:51:21 +0100

    Update profbar.js

--- src/profbar.js
+++ src/profbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './profbar.css';
 import profiles_dtb from './profiles_dtb.json';
+import './profile.css';
+
 
 class ProfBar extends React.Component {
   render() {
 
     const matchResult = this.props.matchResult;
-    let barWidth = 88 - ((88/profiles_dtb.length)*(matchResult))+'vw';
+    let barWidth = 83 - ((83/profiles_dtb.length)*(matchResult))+'vw';
     const c = {width: barWidth};
 
-    let matchPercent = (((88 - ((88/profiles_dtb.length)*(matchResult)))/88)*100)
+    let matchPercent = (((83 - ((83/profiles_dtb.length)*(matchResult)))/83)*100)
     var fixed = matchPercent.toFixed(0);
 
     return (
@@ -30,27 +32,4 @@ class ProfBar extends React.Component {
 }
 export default ProfBar
 
-/*
-const barWidth = 100;
 
-const steps = 10;
-
-class ProfBar extends React.Component {
-
-render(){ 
-
-    const matchResult = this.props.matchResult;
-    let barWidth = matchResult * steps * 2;
-    const c = {width: barWidth};
-
-    barWidth = Math.min(barWidth, barWidth);
-    barWidth = Math.max(barWidth);
-
-        return (
-            <div className="bar" >
-                <div className="profbar" style={c} >
-                 </div>
-            </div>
-        )
-    }
-*/
