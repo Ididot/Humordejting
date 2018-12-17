0ecc1de580e9f2d4da8081a4be2cbd67d4188c60
GeneralSpecifik <44947192+GeneralSpecifik@users.noreply.github.com>
2018-12-17 18:40:39 +0100

    Update progressbar.js

--- src/progressbar.js
+++ src/progressbar.js
@@ -13,7 +13,7 @@ class ProgressBar extends React.Component {
     return (<div className="progress">
 
 	        <div className="progressBar" style={c}>
-	    	{memeProgression+1}
+	    	{memeProgression+1}/10
 	        </div>
         </div>
 
